51f3df15e79e3529f142dc4186855f8d51821ac3
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-06 17:07:35 -0500

    can get presenter notes; other fixes

--- callback.php
+++ callback.php
@@ -4,17 +4,18 @@ require 'vendor/autoload.php';
 $client = new Google_Client();
 $client->setAuthConfigFile('secret/client_secret.json');
 $client->setRedirectUri('https://' . $_SERVER['HTTP_HOST'] . '/google-drive/callback');
-$client->addScope(Google_Service_Drive::DRIVE); //::DRIVE_METADATA_READONLY
+$client->setAccessType('offline'); // https://stackoverflow.com/a/43412638/2624391
+$client->setApprovalPrompt('force');
+$client->addScope(Google_Service_Drive::DRIVE);
 
-if (!isset($_GET['code'])) {
-  $auth_url = $client->createAuthUrl();
-  header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
-} else {
-  $client->authenticate($_GET['code']);
-  $access_token = $client->getAccessToken();
-  setcookie("GoogleAPICredentials", json_encode($access_token), time()+(60*60*24*90), "/");
-   
+if (isset($_GET['code'])) {
+  $access_token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+  // $client->setAccessToken($access_token); // need to do this on index.php
+  setcookie("GoogleAPICredentials", json_encode($access_token), time()+(60*60*24*1), "/");
   $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/google-drive/';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+} else {
+	$auth_url = $client->createAuthUrl();
+  header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 }
 ?>
--- index.php
+++ index.php
@@ -8,38 +8,52 @@ $client->setAuthConfig('secret/client_secret.json');
 $client->addScope(Google_Service_Drive::DRIVE);
 
 if (isset($_COOKIE['GoogleAPICredentials'])) {
-	$access_token = $_COOKIE['GoogleAPICredentials'];
+	$access_token = json_decode($_COOKIE['GoogleAPICredentials'], true);
+	if (!is_array($access_token)) {
+		callback();
+	}
 	$client->setAccessToken($access_token);
 	if ($client->isAccessTokenExpired()) {
-		$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-		$access_token2 = $client->getAccessToken();
-    $access_token = array_merge($access_token, $access_token2);
-		setcookie("GoogleAPICredentials", json_encode($access_token), time()+(60*60*24*90), "/");
-	}
+    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+    setcookie("GoogleAPICredentials", json_encode($client->getAccessToken()), time()+(60*60*24*1), "/");
+  }
 	if (!isset($_GET['id'])) {
 		exit("Provide a presentation id");
 	} else {
 		$presentationId = $_GET['id'];
 	}
+
+	// TO PRINT SLIDE PRESENTER NOTES
 	// $service = new Google_Service_Slides($client);
 	// $presentation = $service->presentations->get($presentationId);
 	// $slides = $presentation->getSlides();
-	// foreach ($slides as $i => $slide) {
-	//   // Print columns A and E, which correspond to indices 0 and 4.
-	//   printf("- Slide #%s contains %s elements.\n", $i + 1, count($slide->getPageElements()));
+	// foreach ($slides as $slide) {
+	// 	$note_id = $slide->slideProperties->notesPage->notesProperties->speakerNotesObjectId;
+	// 	foreach ($slide->slideProperties->notesPage as $obj) {
+	// 		if ($obj->objectId === $note_id) {
+	// 			var_dump($obj->shape->text->textElements[1]->textRun->content);
+	// 		}
+	// 	}
 	// }
+
+	// TO OUTPUT PDF OF PRESENTATION
+	// header('Content-Type: application/pdf');
+	// $driveService = new Google_Service_Drive($client);
+	// $response = $driveService->files->export($presentationId, 'application/pdf', array('alt' => 'media'));
+	// $content = $response->getBody()->getContents();
+	// echo $content;
+
+	// have to iterate over individual slides to export as images; might be easier to convert PDFs to images on our end
 	// for ($i=0; $i < count($slides); $i++) {
 	// 	$id = $slides[$i]->objectId;
 	// 	$export = 'https://docs.google.com/presentation/d/' + $presentationId +'/export/png?id=' + $presentationId + '&pageid=' + $id;
 	// }
-	header('Content-Type: application/pdf');
-	$driveService = new Google_Service_Drive($client);
-	$response = $driveService->files->export($presentationId, 'application/pdf', array(
-	    'alt' => 'media'));
-	$content = $response->getBody()->getContents();
-	echo $content;
 } else {
-  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/google-drive/callback';
+	callback();
+}
+function callback() {
+	$redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/google-drive/callback';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  exit();
 }
 ?>
