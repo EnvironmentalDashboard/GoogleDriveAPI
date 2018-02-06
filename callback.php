7b06389ddd705b74eb9da0ce2702e54acb8b2968
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-05 23:45:26 -0500

    it's working now

--- callback.php
+++ callback.php
@@ -2,19 +2,19 @@
 require 'vendor/autoload.php';
 
 $client = new Google_Client();
-$client->setAuthConfigFile('client_id.json');
-$client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/GoogleDriveAPI/callback.php');
+$client->setAuthConfigFile('secret/client_secret.json');
+$client->setRedirectUri('https://' . $_SERVER['HTTP_HOST'] . '/google-drive/callback');
 $client->addScope(Google_Service_Drive::DRIVE); //::DRIVE_METADATA_READONLY
 
-if (! isset($_GET['code'])) {
+if (!isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
   $client->authenticate($_GET['code']);
   $access_token = $client->getAccessToken();
-  file_put_contents("credentials.json", json_encode($access_token));
+  setcookie("GoogleAPICredentials", json_encode($access_token), time()+(60*60*24*90), "/");
    
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/GoogleDriveAPI/';
+  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/google-drive/';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 ?>
--- index.php
+++ index.php
@@ -5,19 +5,19 @@ $client = new Google_Client();
 $client->setAuthConfig('secret/client_secret.json');
 $client->addScope(Google_Service_Drive::DRIVE);
 
-if (file_exists("credentials.json")) {
-	$access_token = (file_get_contents("credentials.json"));
+if (isset($_COOKIE['GoogleAPICredentials'])) {
+	$access_token = $_COOKIE['GoogleAPICredentials'];
 	$client->setAccessToken($access_token);
 	//Refresh the token if it's expired.
 	if ($client->isAccessTokenExpired()) {
 		$client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-		file_put_contents($credentialsPath, json_encode($client->getAccessToken()));
+		setcookie("GoogleAPICredentials", json_encode($client->getAccessToken()), time()+(60*60*24*90), "/");
 	}
 	$drive_service = new Google_Service_Drive($client);
 	$files_list = $drive_service->files->listFiles(array())->getFiles(); 
 	echo json_encode($files_list);
 } else {
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/GoogleDriveAPI/callback.php';
+  $redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . '/google-drive/callback';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 ?>
